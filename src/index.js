91a4a5f10c723d1c556025fdad12e0bc8d7bc9f1
Nikita <ussnikita209@mail.ru>
2021-08-18 18:48:40 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsStack = []
+  for (let ch of str) {
+    for (let bracket of bracketsConfig) {
+      if (ch == bracket[0]) {
+        if (bracket[0] == bracket[1]) {
+          if (bracketsStack.length == 0) {
+            bracketsStack.push(bracket[0])
+            break
+          } else {
+            const lastBracket = bracketsStack.pop()
+            if (lastBracket != bracket[0]) {
+              bracketsStack.push(lastBracket)
+              bracketsStack.push(bracket[0])
+            }
+            break
+          }
+        }
+        bracketsStack.push(bracket[1])
+        break
+      }
+      if (ch == bracket[1]) {
+        if (ch != bracketsStack.pop()) return false
+        break
+      }
+    }
+  }
+  
+  return bracketsStack.length == 0 ? true : false
 }
